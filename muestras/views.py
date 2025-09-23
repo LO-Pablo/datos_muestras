70874124acdb4f863409efdf0e9e8177203a1013
MThem97 <mthem97@gmail.com>
2025-09-23 12:10:00 +0200

    añadir urls y view de main y muestras

--- muestras/views.py
+++ muestras/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse
 from .models import Muestra
 from django.template import loader
 # Create your views here.
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
 def muestras_todas(request):
     muestras = Muestra.objects.all().values()
-    template = loader.get_template('muestras_all.html')
+    template = loader.get_template('muestras_todas.html')
     context = {
         'muestras': muestras,
     }
