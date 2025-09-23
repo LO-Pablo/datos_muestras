f92af726cc82b3824777e20560e7b52d408ef15e
MThem97 <mthem97@gmail.com>
2025-09-23 11:53:48 +0200

    creación view, template y html de muestras

--- muestras/views.py
+++ muestras/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Muestra
+from django.template import loader
 # Create your views here.
+def muestras_todas(request):
+    muestras = Muestra.objects.all().values()
+    template = loader.get_template('muestras_all.html')
+    context = {
+        'muestras': muestras,
+    }
+    return HttpResponse(template.render(context, request))
