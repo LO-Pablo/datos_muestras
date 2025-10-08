8bbc9fe85cdd5c8a9ae6bc747beaba2d62648ad4
MThem97 <mthem97@gmail.com>
2025-10-08 16:45:09 +0200

    creación de modelos para envios, localizacion y estudio

--- muestras/models.py
+++ muestras/models.py
@@ -4,15 +4,15 @@ from django.utils import timezone
 
 class Muestra(models.Model):
     # Campos del modelo Muestra
-    id_individuo = models.CharField(max_length=20)
-    nom_lab = models.CharField(max_length=100)
+    id_individuo = models.CharField(max_length=20, unique=True)
+    nom_lab = models.CharField(max_length=100, unique=True)
     id_material = models.CharField(max_length=20)
     volumen_actual = models.FloatField()
-    unidad_volumen = models.CharField(max_length=10)
+    unidad_volumen = models.CharField(max_length=15)
     concentracion_actual = models.FloatField()
-    unidad_concentracion = models.CharField(max_length=10)
+    unidad_concentracion = models.CharField(max_length=15)
     masa_actual = models.FloatField()
-    unidad_masa = models.CharField(max_length=10)
+    unidad_masa = models.CharField(max_length=15)
     fecha_extraccion = models.DateField()
     fecha_llegada = models.DateField()
     observaciones = models.TextField(blank=True, null=True)
@@ -20,6 +20,8 @@ class Muestra(models.Model):
                                       choices=[('CONG','Congelado'), ('REF','Refrigerado'), ('AMB','Ambiente')])
     centro_procedencia = models.CharField(max_length=100)
     lugar_procedencia = models.CharField(max_length=100)
+    estado_actual = models.CharField(max_length=50, default='Disponible',
+                                        choices=[('DISP','Disponible'), ('ENV','Enviada'), ('ENVP','Parcialmente enviada'), ('DEST','Destruida')])
 
     class Meta:
         # Definición de permisos personalizados para el modelo Muestra
@@ -31,3 +33,47 @@ class Muestra(models.Model):
         ]
     def __str__(self):
         return f"Muestra {self.id_individuo} - {self.nom_lab}"
+
+class Localizacion(models.Model):
+    # Campos del modelo Localizacion, que referencia a una muestra
+    id_individuo = models.ForeignKey(Muestra, to_field='id_individuo', on_delete=models.CASCADE, related_name='localizaciones_individuo')
+    nom_lab = models.ForeignKey(Muestra, to_field='nom_lab', on_delete=models.CASCADE, related_name='localizaciones_nom_lab')
+    congelador = models.CharField(max_length=50)
+    estante = models.CharField(max_length=50)
+    posicion_rack_estante = models.CharField(max_length=50)
+    rack = models.CharField(max_length=50)
+    posicion_caja_rack = models.CharField(max_length=50)
+    caja = models.CharField(max_length=50)
+    subposicion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.congelador} - {self.estante} - {self.posicion_rack_estante} - {self.rack} - {self.posicion_caja_rack} - {self.caja} - {self.subposicion}"
+    
+class Estudio(models.Model):
+    # Campos del modelo Estudio, que referencia a una muestra
+    id_individuo = models.ForeignKey(Muestra, to_field='id_individuo', on_delete=models.CASCADE, related_name='estudio_individuo')
+    nom_lab = models.ForeignKey(Muestra, to_field='nom_lab', on_delete=models.CASCADE, related_name='estudio_nom_lab')
+    id_estudio = models.CharField(max_length=20)
+    referencia_estudio = models.CharField(max_length=100)
+    nombre_estudio = models.CharField(max_length=100)
+    descripcion_estudio = models.TextField(blank=True, null=True)
+    fecha_inicio_estudio = models.DateField()
+    fecha_fin_estudio = models.DateField(blank=True, null=True)
+    investigador_principal = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Estudio {self.nombre_estudio} para Muestra {self.id_individuo} - {self.nom_lab}"
+
+class Envio(models.Model):
+    # Campos del modelo Envio, que referencia a una muestra
+    id_individuo = models.ForeignKey(Muestra, to_field='id_individuo', on_delete=models.CASCADE, related_name='envio_individuo')
+    nom_lab = models.ForeignKey(Muestra, to_field='nom_lab', on_delete=models.CASCADE, related_name='envio_nom_lab')
+    volumen_enviado = models.FloatField()
+    unidad_volumen_enviado = models.CharField(max_length=15)
+    concentracion_enviada = models.FloatField()
+    unidad_concentracion_enviada = models.CharField(max_length=15)
+    centro_destino = models.CharField(max_length=100)
+    lugar_destino = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Envio de Muestra {self.id_individuo} - {self.nom_lab} el {self.fecha_envio}"
