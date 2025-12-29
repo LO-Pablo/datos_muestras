9fb353063db475095fd25c82ed996a42e6d6382f
MThem97 <mthem97@gmail.com>
2025-12-29 21:17:58 +0100

    comentados templates y filtros personalizados

--- muestras/templatetags/muestras_filters.py
+++ muestras/templatetags/muestras_filters.py
@@ -1,13 +1,13 @@
 from django import template
-
+# Filtros personalizados para plantillas de Django
 register = template.Library()
-
+# Filtro para verificar si una cadena comienza con un prefijo específico
 @register.filter
 def startswith(text, starts):
     if isinstance(text, str):
         return text.startswith(starts)
     return False
-
+# Filtro para obtener un valor de un diccionario dado una clave
 @register.filter
 def get_item(dictionary, key):
     try:
